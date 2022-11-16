e3d3a36f58f485de410675a8f7dc374ab9b9ac85
Jeancarlo Carlo <103619912+Jean5316@users.noreply.github.com>
2022-11-16 01:51:21 +0000

    atualização com função main()

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ def corrigir_nome(site):
         return site
 
 def inicia_novamente(atz_exc):
-         
-                if atz_exc == 's' or atz_exc == 'S':
-                        sites = corrigir_nome(input('Qual site: '))
-                        return sites
-                elif atz_exc == 'n' or atz_exc == 'N':
+                atz_exc = atz_exc.lower()
+                if atz_exc == 's':
+                        main()
+                        
+                elif atz_exc == 'n':
                         fim = print("Programa Finalizado")
                         return fim
                 else:
-                        while atz_exc != 's' or 'n' or atz_exc != 'S' or 'N':
+                        while atz_exc != 's' or 'n':
                                 invalido = print("Opção invalida!")
-                                inicia_novamente(input("Quer consultar mais site? "))
+                                inicia_novamente(input("Quer consultar mais site?(s/n) "))
                                 return invalido 
                                 
     
@@ -38,29 +38,29 @@ status_code_erro_cliente = [400,401,402,403,404,405,406,407,408,409,410,411,412,
                         416,417,418,421,422,423,424,425,426,428,429,431,451]
 status_code_erro_servidor = [500,501,502,503,504,505,506,507,508,509,510,511]
 
-sites = corrigir_nome(input("Qual o site(s): "))
-urls = sites
-
-
-for site in urls:
+def main():
+        print("insira a url separada por virgual")
+        #pega url
+        urls = corrigir_nome(input("Quais os site(s): "))
         
-                #verifica o ponto
-                if "." not in site:
-                        print(f"Verifique a URL do {site}.")
-                 #verifica http ou https       
-                else: 
-                        if "http"  not in site:
-                                site = f"http://{site}." 
-
-                        try: 
-                                r = requests.get(site)
-                                if r.status_code in status_code_sucesso or status_code_redirecionamento:
-                                        site_on = print(f"{site} online, status code {r.status_code}.")
-                                                                                        
-                                elif r.status_code in status_code_erro_cliente or status_code_erro_servidor:
-                                        site_off = print(f"{site} offline, status code {r.status_code}.") 
-                                                                                       
-                        except:
-                                        nao_encontrou = print(f"{site} invalido ou sem resposta.")
+        for site in urls:
+                
+                        #verifica o ponto
+                        if "." not in site:
+                                print(f"Verifique a URL do {site}.")
+                         #verifica http ou https       
+                        else: 
+                                if "http"  not in site:
+                                        site = f"http://{site}." 
         
-inicia_novamente(input("Quer consultar mais site?(s/n)"))
+                                try: 
+                                        r = requests.get(site)
+                                        if r.status_code in status_code_sucesso or status_code_redirecionamento:
+                                                print(f"{site} online, status code {r.status_code}.")
+                                                                                               
+                                except:
+                                        print(f"{site} invalido ou sem resposta.")
+        
+        inicia_novamente(input("Quer consultar mais algum site?(s/n) "))                                          
+
+main()
